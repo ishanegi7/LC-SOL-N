dc295377f820c4996f229ecc4f4728f253160078
ishanegi7 <negiishaan89@gmail.com>
2026-07-29 23:26:35 +0530

    implemented solution for reverse string

---
+++ Reverse Integer/code.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revNum = 0;
+
+        while (x != 0) {
+            int dig = x % 10;
+            x /= 10;
+
+            if (revNum > INT_MAX / 10 || 
+               (revNum == INT_MAX / 10 && dig > 7))
+                return 0;
+
+            if (revNum < INT_MIN / 10 || 
+               (revNum == INT_MIN / 10 && dig < -8))
+                return 0;
+
+            revNum = revNum * 10 + dig;
+        }
+
+        return revNum;
+    }
+};
